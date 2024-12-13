fc5d750833d250c71021707b4754611c1635704a
Devansh Pareek <devanshpareek863@gmail.com>
2024-12-14 03:01:26 +0530

    minor changes

--- src/constants/address.js
+++ src/constants/address.js
@@ -1,7 +1,7 @@
 export const LOCATION_LINK = "https://maps.app.goo.gl/rYDpQrkv6VLPaRP3A";
-export const ADDRESS_LINE1 = 'Basement,C-78'
-export const ADDRESS_LINE2 = "Below Kotak Mahindra Bank, Block C, Malviya Nagar";
-export const ADDRESS_LINE3 = "New Delhi, Delhi, 110017";
+export const ADDRESS_LINE1 = 'Basement, C - 78';
+export const ADDRESS_LINE2 = 'Below Kotak Mahindra Bank, Block C';
+export const ADDRESS_LINE3 = 'Malviya Nagar, New Delhi, Delhi, 110017';
 export const FULL_ADDRESS =
     "Basement, C - 78, Below Kotak Mahindra Bank, Block C, Malviya Nagar, New Delhi, Delhi 110017";
 export const LOCATION_COORDINATES_GOOGLE_MAP = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d112099.78170915024!2d77.14386648906245!3d28.59623129723165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce3bbdbf56ff5%3A0x59ccd701a625663!2sSkinfollics%20skin%20and%20hair%20transplant%20clinic!5e0!3m2!1sen!2sin!4v1734124385890!5m2!1sen!2sin";
